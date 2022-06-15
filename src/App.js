4006ed8da4c0d19ceaef5427b9f1cef0fd5dfcf6
Federico Amico <fede.amico98@gmail.com>
2022-06-15 16:31:52 -0300

    first desing of de presentation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import './general-styles.css';
 import Header from './components/header/Header.js';
+import Presentation from './components/presentation/Presentation';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Presentation />
     </div>
   );
 }
---
+++ src/components/presentation/Presentation.js
@@ -0,0 +1,12 @@
+import './Presentation.css';
+
+const Presentation = () => {
+  return (
+    <div className="presentation-container">
+      <h1 className="presentation-h1">&lt; AMICO, FEDERICO MARTIN /&gt;</h1>
+       <p className="presentation-p">Web and Backend Developer</p>
+    </div>
+  )
+}
+
+export default Presentation;
