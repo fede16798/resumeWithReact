c7bb7d5e274d08ee25c9928e0ec45d536e270760
Federico Amico <fede.amico98@gmail.com>
2022-06-15 13:08:15 -0300

    initial structure of header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import './general-styles.css';
+import Header from './components/header/Header.js';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
