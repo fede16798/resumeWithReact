b5f531a31948ef66979fbdea76bc56d82879b360
Federico Amico <fede.amico98@gmail.com>
2022-06-16 13:02:22 -0300

    typeWriter npm added

--- src/components/presentation/Presentation.js
+++ src/components/presentation/Presentation.js
@@ -2,11 +2,28 @@ import './Presentation.css';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
+import Typewriter from "typewriter-effect";
+
 const Presentation = () => {
+
   return (
     <div className="presentation-container">
-      <h1 className="presentation-h1">&lt; AMICO, FEDERICO MARTIN /&gt;</h1>
-      <p className="presentation-p">Web and Backend Developer</p>
+      <h1 className="presentation-h1">
+      <Typewriter
+        onInit={(typewriter)=> {
+        typewriter
+        .typeString("< AMICO, FEDERICO MARTIN />")
+        .pauseFor(300)
+        .deleteChars(10)
+        .typeString(" MARTIN />")
+        .start();
+        }}
+        options = {{
+          loop: true,
+        }}
+      />
+     </h1>
+      <p className="presentation-p">Fullstack Developer</p>
       <div className="icons-container">
           <a href="https://www.linkedin.com/in/federico-martin-amico" rel="noreferrer" target="_blank" className='icons-container__a'> 
             <LinkedInIcon sx={{fontSize: 52}} className='icons-container__icon' /> 
