56d2e7e873a716d08584a20806638932579c5918
Federico Amico <fede.amico98@gmail.com>
2022-06-16 12:46:16 -0300

    Material UI package added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './general-styles.css';
 import Header from './components/header/Header.js';
 import Presentation from './components/presentation/Presentation';
 
-
 function App() {
   return (
     <div className="App">
--- src/components/presentation/Presentation.js
+++ src/components/presentation/Presentation.js
@@ -1,10 +1,21 @@
 import './Presentation.css';
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ArrowDropDownRoundedIcon from '@mui/icons-material/ArrowDropDownRounded';
 const Presentation = () => {
   return (
     <div className="presentation-container">
       <h1 className="presentation-h1">&lt; AMICO, FEDERICO MARTIN /&gt;</h1>
-       <p className="presentation-p">Web and Backend Developer</p>
+      <p className="presentation-p">Web and Backend Developer</p>
+      <div className="icons-container">
+          <a href="https://www.linkedin.com/in/federico-martin-amico" rel="noreferrer" target="_blank" className='icons-container__a'> 
+            <LinkedInIcon sx={{fontSize: 52}} className='icons-container__icon' /> 
+          </a>
+          <a href="https://github.com/fede16798" rel="noreferrer" target="_blank" className='icons-container__a'> 
+            <GitHubIcon sx={{fontSize: 47}} className='icons-container__icon' /> 
+          </a>
+      </div>
+      <ArrowDropDownRoundedIcon sx={{fontSize: 70}} className='icons-container__icon arrow'/>
     </div>
   )
 }
