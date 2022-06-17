20e588677417af84d1b034a67a35e003253614db
Federico Amico <fede.amico98@gmail.com>
2022-06-17 13:46:15 -0300

    about section and its structure created

---
+++ src/components/aboutMe/AboutMe.js
@@ -0,0 +1,24 @@
+import './AboutMe.css';
+
+const AboutMe = () => {
+  return (
+    <div className='about-section'>
+      <p>prueba</p>
+      <div className='about-container'>
+        <h3 className='about-container__h3'>About Me</h3>
+        <p className='about-container__p'>
+          Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adi Lorem Lorem ipsum dolor sit amet, consectetur
+          Lorem ipsum dolor sit amet, consecteloLorem
+          tur adip Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consectetur
+          Lorem ipsum dolor sit amet, consect 
+        </p>
+        <section className='button-section'>
+          <p className='button-section__download'>Download CV</p>
+          <p className='button-section__contact'>Contact me</p>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default AboutMe;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,13 @@
 import Header from '../header/Header.js';
 import Presentation from '../presentation/Presentation.js';
+import AboutMe from '../aboutMe/AboutMe.js';
 
 const HomePage = () => {
 	return (
 		<>
 			<Header />
 			<Presentation />
+			<AboutMe />
 		</>
 	)
 }
