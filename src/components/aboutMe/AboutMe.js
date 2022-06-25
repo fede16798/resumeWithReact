e89efb452d31c81ef88725c433fdf974d1ba335e
Federico Amico <fede.amico98@gmail.com>
2022-06-25 13:56:35 -0300

    styles yo the bottons added. Img added

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -1,9 +1,12 @@
 import './AboutMe.css';
+import resumeImg from '../../assets/resume-image.jpg'
 
 const AboutMe = () => {
   return (
     <div className='about-section'>
-      <p>prueba</p>
+      <section className='img-container'>
+        <img src={resumeImg} alt='resume'/>
+      </section>
       <div className='about-container'>
         <h3 className='about-container__h3'>About Me</h3>
         <p className='about-container__p'>
@@ -13,8 +16,8 @@ const AboutMe = () => {
           Lorem ipsum dolor sit amet, consect 
         </p>
         <section className='button-section'>
-          <p className='button-section__download'>Download CV</p>
-          <p className='button-section__contact'>Contact me</p>
+          <p className='button-section__download'><span className='button-section__span'>Download CV</span></p>
+          <p className='button-section__contact'><span className='button-section__span'>Contact me</span></p>
         </section>
       </div>
     </div>
