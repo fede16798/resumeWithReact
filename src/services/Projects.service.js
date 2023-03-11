24730cfc3fead356d71143a7e8c520bee7389f8f
Federico <fede.amico98@gmail.com>
2023-03-11 13:57:41 -0300

    correction of link to get the project info

--- src/services/Projects.service.js
+++ src/services/Projects.service.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const getProjects = async (language) => {
     if (language == 'en') {
-        return await axios.get('./projectsEnsss.json');
+        return await axios.get('./projectsEn.json');
     } else {
-        return await axios.get('./projectsEssss.json');
+        return await axios.get('./projectsEs.json');
     }
 }
 
